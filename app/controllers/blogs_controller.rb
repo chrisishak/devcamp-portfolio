708753c5cb5b870ea396f3844f4fc042def92c3b
Chris <christophe@Christophes-MacBook-Pro.local>
2022-12-06 12:52:41 +0200

    Test

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,9 +3,8 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.all
-  end
-
+    @blogs = Blog.limit(2)
+  elimit(2)
   # GET /blogs/1 or /blogs/1.json
   def show
   end
