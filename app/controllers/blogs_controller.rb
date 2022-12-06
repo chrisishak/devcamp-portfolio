ecfdfe593195af6639389420e66fd2c65241348a
Chris <christophe@Christophes-MacBook-Pro.local>
2022-12-06 12:54:15 +0200

    revert changes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,8 +3,9 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.limit(2)
-  elimit(2)
+    @blogs = Blog.all
+  end
+
   # GET /blogs/1 or /blogs/1.json
   def show
   end
